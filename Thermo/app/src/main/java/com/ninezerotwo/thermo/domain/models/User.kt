e8ed52417440dde84182b311d84d6862f0365c35
Azat Gilyazov <azatpscl@gmail.com>
2021-07-01 20:00:47 +0300

    [2_1] feature: add dependencies for hild and retrofit

--- Thermo/app/src/main/java/com/ninezerotwo/thermo/domain/models/User.kt
+++ Thermo/app/src/main/java/com/ninezerotwo/thermo/domain/models/User.kt
@@ -2,6 +2,7 @@ package com.ninezerotwo.thermo.domain.models
 
 data class User(
     val id: String = "",
+    val username: String = "",
     val email: String = "",
     val firstName: String = "",
     val lastName: String = "",
