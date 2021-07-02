7875db43073a751cf1f349b7f7b32320e9d6e170
Azat Gilyazov <azatpscl@gmail.com>
2021-07-02 23:12:57 +0300

    [2_1] bugfix: replace hilt app annotation

--- Thermo/app/src/main/java/com/ninezerotwo/thermo/ui/AppActivity.kt
+++ Thermo/app/src/main/java/com/ninezerotwo/thermo/ui/AppActivity.kt
@@ -1,12 +1,41 @@
 package com.ninezerotwo.thermo.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.ninezerotwo.thermo.R
+import com.ninezerotwo.thermo.data.network.AuthApi
+import com.ninezerotwo.thermo.data.network.dto.UserSignUpDto
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class AppActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var authApi: AuthApi
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        runBlocking {
+            val response = authApi.signUp(
+                UserSignUpDto(
+                    "sdfds@sdf.er",
+                    "sdfs",
+                    "sdfsef",
+                    "sdfsf",
+                    "sdf234"
+                )
+            )
+            Log.d("antee-tag",response.toString())
+        }
     }
 }
