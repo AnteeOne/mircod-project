62bb43cd2c38cf3af89f5259ad680ccd99e4095f
Azat Gilyazov <azatpscl@gmail.com>
2021-07-08 22:58:41 +0300

    [8] feature: add getting battery and temperature logic

--- Thermo/app/src/main/java/com/ninezerotwo/thermo/device/repositories/ThermoRepository.kt
+++ Thermo/app/src/main/java/com/ninezerotwo/thermo/device/repositories/ThermoRepository.kt
@@ -3,39 +3,95 @@ package com.ninezerotwo.thermo.device.repositories
 import android.bluetooth.BluetoothGatt
 import com.clj.fastble.BleManager
 import com.clj.fastble.callback.BleGattCallback
+import com.clj.fastble.callback.BleNotifyCallback
+import com.clj.fastble.callback.BleReadCallback
 import com.clj.fastble.data.BleDevice
 import com.clj.fastble.exception.BleException
+import com.ninezerotwo.thermo.device.bluetooth.BleConstants
+import com.ninezerotwo.thermo.device.bluetooth.ByteConverter
 import com.ninezerotwo.thermo.domain.repositories.IThermoRepository
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.onFailure
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
 
 class ThermoRepository @Inject constructor(
     private val manager: BleManager
-): IThermoRepository {
+) : IThermoRepository {
 
-    override suspend fun connectToDevice(deviceMac: String): Boolean = suspendCancellableCoroutine { cor ->
-        manager.connect(deviceMac,object: BleGattCallback(){
-            override fun onStartConnect() {}
+    private var savedBleDevice: BleDevice? = null
 
-            override fun onConnectFail(bleDevice: BleDevice?, exception: BleException?) {
-                cor.resumeWith(Result.failure(IllegalStateException(exception?.description)))
-            }
+    override suspend fun connectToDevice(deviceMac: String): Boolean =
+        suspendCancellableCoroutine { cor ->
+            manager.connect(deviceMac, object : BleGattCallback() {
+                override fun onStartConnect() {}
 
-            override fun onConnectSuccess(
-                bleDevice: BleDevice?,
-                gatt: BluetoothGatt?,
-                status: Int
-            ) {
-                cor.resumeWith(Result.success(true))
-            }
+                override fun onConnectFail(bleDevice: BleDevice?, exception: BleException?) {
+                    cor.resumeWith(Result.failure(IllegalStateException(exception?.description)))
+                }
 
-            override fun onDisConnected(
-                isActiveDisConnected: Boolean,
-                device: BleDevice?,
-                gatt: BluetoothGatt?,
-                status: Int
-            ) {}
+                override fun onConnectSuccess(
+                    bleDevice: BleDevice?,
+                    gatt: BluetoothGatt?,
+                    status: Int
+                ) {
+                    savedBleDevice = bleDevice
+                    cor.resumeWith(Result.success(true))
+                }
 
-        })
+                override fun onDisConnected(
+                    isActiveDisConnected: Boolean,
+                    device: BleDevice?,
+                    gatt: BluetoothGatt?,
+                    status: Int
+                ) {
+                    savedBleDevice = null
+                }
+
+            })
+        }
+
+    override suspend fun getBatteryLevel(): Int = suspendCancellableCoroutine { cor ->
+        manager.read(savedBleDevice,
+            BleConstants.BATTERY_SERVICE_UUID,
+            BleConstants.BATTERY_CHAR_UUID,
+            object: BleReadCallback() {
+
+                override fun onReadSuccess(data: ByteArray) {
+                    cor.resumeWith(Result.success(data[0].toInt()))
+                }
+
+                override fun onReadFailure(exception: BleException) {
+                    cor.resumeWith(Result.failure(IllegalStateException(exception.description)))
+                }
+
+            })
     }
+
+    override fun getTemperature(): Flow<Float> = callbackFlow {
+        manager.notify(savedBleDevice,
+            BleConstants.TEMP_SERVICE_UUID,
+            BleConstants.TEMP_CHAR_UUID,
+            object: BleNotifyCallback(){
+                override fun onNotifySuccess() {}
+
+                override fun onNotifyFailure(exception: BleException) {
+                    cancel(CancellationException("Temperature API Error"))
+                }
+
+                override fun onCharacteristicChanged(data: ByteArray) {
+                    trySendBlocking(ByteConverter.toTemperature(data[0],data[1])).onFailure {
+                        cancel(CancellationException("Temperature API Error"))
+                    }
+                }
+
+            })
+        
+    }
+
+
 }
